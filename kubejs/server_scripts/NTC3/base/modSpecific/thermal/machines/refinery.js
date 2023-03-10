1f866d2a461a7397a1bdb63dda6865e0a204cf19
MechOrigin <matt__tatum@msn.com>
2023-03-10 09:29:29 -0800

    push changes before oil update

--- kubejs/server_scripts/NTC3/base/modSpecific/thermal/machines/refinery.js
+++ kubejs/server_scripts/NTC3/base/modSpecific/thermal/machines/refinery.js
@@ -63,7 +63,7 @@ onEvent('recipes', (event) => {
             input: Fluid.of('thermal:latex', 90),
             outputs: [
                 Fluid.of('secretly_complicated:rubber_fluid_still', 90),
-                Item.of('secretly_complicated:small_rubber_dust').withChance(0.10),
+                Item.of('secretly_complicated:rubber_small_dust').withChance(0.10),
             ],
             energy: 6000,
             id: `${id_prefix}raw_latex_to_rubber_fluid`
