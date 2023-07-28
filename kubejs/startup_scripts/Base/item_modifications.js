9ce1d353a9ef3dddad6ad4518b28225a1e51bee9
MechOrigin <matt__tatum@msn.com>
2023-07-27 21:54:00 -0700

    large furnace

--- kubejs/startup_scripts/Base/item_modifications.js
+++ kubejs/startup_scripts/Base/item_modifications.js
@@ -5,13 +5,17 @@ onEvent('item.modification', event => {
       item.maxDamage = 64 //change from 9 durability to 64
     })
 
+
+
     event.modify('kubejs:tool_hand_saw', item => {
       item.maxDamage = 64 //change from 9 durability to 64
-      item.craftingRemainder = 'kubejs:tool_hand_saw'
+      item.craftingRemainder = Item.of('kubejs:tool_hand_saw', `{Damage:0}`)
+      item.uses = 64
     })
 
     event.modify('kubejs:tool_hand_file', item => {
       item.maxDamage = 64 //change from 9 durability to 64
       item.craftingRemainder = 'kubejs:tool_hand_file'
+      item.uses = 64
     })
   })
