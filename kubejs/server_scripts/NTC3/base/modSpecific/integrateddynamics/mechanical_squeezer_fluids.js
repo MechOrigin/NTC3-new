4f92c01e3e91a5ecb473b0fbceb655c5ec88a97f
MechOrigin <matt__tatum@msn.com>
2023-07-31 14:18:53 -0700

    more multiblocks

--- kubejs/server_scripts/NTC3/base/modSpecific/integrateddynamics/mechanical_squeezer_fluids.js
+++ kubejs/server_scripts/NTC3/base/modSpecific/integrateddynamics/mechanical_squeezer_fluids.js
@@ -1,34 +1,34 @@
-onEvent('recipes', function (event) {
-    // Item Inputs
-    var data = {
-        recipes: [
-            {
-                input: 'minecraft:acacia_log',
-                output: 'minecraft:stripped_acacia_log',
-                fluidOutput: 'thermal:latex',
-                amount: 10,
-                count: 1,
-                duration: 30
-            }
-        ]
-    };
+// onEvent('recipes', function (event) {
+//     // Item Inputs
+//     var data = {
+//         recipes: [
+//             {
+//                 input: 'minecraft:acacia_log',
+//                 output: 'minecraft:stripped_acacia_log',
+//                 fluidOutput: 'thermal:latex',
+//                 amount: 10,
+//                 count: 1,
+//                 duration: 30
+//             }
+//         ]
+//     };
 
-    data.recipes.forEach((recipe) => {
-        event.custom({
-            type: 'integrateddynamics:mechanical_squeezer',
-            item: recipe.input,
-            result: {
-                fluid: {
-                    fluid: recipe.fluidOutput,
-                    amount: recipe.amount
-                },
-                item: {
-                    item: recipe.output,
-                    count: recipe.count
-                }
-            },
-            duration: recipe.duration
-        });
-    });
+//     data.recipes.forEach((recipe) => {
+//         event.custom({
+//             type: 'integrateddynamics:mechanical_squeezer',
+//             item: recipe.input,
+//             result: {
+//                 fluid: {
+//                     fluid: recipe.fluidOutput,
+//                     amount: recipe.amount
+//                 },
+//                 item: {
+//                     item: recipe.output,
+//                     count: recipe.count
+//                 }
+//             },
+//             duration: recipe.duration
+//         });
+//     });
 
-});
+// });
